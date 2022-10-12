99a17c37829ca68602d2f7e854d698007f37f709
Fevronia Gjini <fevronigjini1@gmail.com>
2022-10-13 00:46:26 +0200

    did the auth for the users, tryed on thunder worked

--- src/server.ts
+++ src/server.ts
@@ -1,67 +1,86 @@
-import  express  from "express";
-import cors from 'cors'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcryptjs'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
-dotenv.config()
+dotenv.config();
 
-console.log(bcrypt)
+console.log(bcrypt);
 
+const app = express();
+app.use(cors());
+app.options("*", cors);
+app.use(express.json());
+const prisma = new PrismaClient();
 
+const SECRET = "ABC";
 
-
-const app = express()
-app.use(cors())
-app.options('*', cors)
-app.use(express.json())
-const prisma = new PrismaClient()
-
-const SECRET = process.env.SECRET!
-
-function getToken (id: number) {
+function getToken(id: number) {
   return jwt.sign({ id: id }, SECRET, {
-    expiresIn: '30 days'
-  })
+    expiresIn: "30 days",
+  });
 }
 
-const port = 5636
+async function getCurrentUser (token :string){
+    // @ts-ignore
+    const {id} = jwt.verify(token, SECRET);
+    const user = await prisma.user.findUnique({where: {id :id} });
+  return user;
+}
 
-app.get('/users', async (req, res)=>{
+const port = 5637;
 
-})
-app.post("/sign-up", async (req, res)=>{
-   const user =  await prisma.user.create({ 
-    data:{
-        name:req.body.name,
-        lastName:req.body.lastName,
-        email:req.body.email,
-        password: bcrypt.hashSync(req.body.password)
+app.get("/users", async (req, res) => {});
 
-    }})
-    res.send(user)
-    
-})
-app.post('/sign-in', async(req,res)=>{
-    const user = await prisma.user.findUnique({
-        where:{email:req.body.email},
-    })
-    if(user && bcrypt.compareSync(req.body.password,user.password)){
-        res.send(user)
-    } else{
-        res.status(400).send({error: 'Invalid email/password'});
-        
+app.post("/sign-up", async (req, res) => {
+  try {
+    const match = await prisma.user.findUnique({
+      where: { email: req.body.email },
+    });
 
+    if (match) {
+      res.status(400).send({ error: "This account already exist!" });
+    } else {
+      const newUser = await prisma.user.create({
+        data: {
+          name: req.body.name,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password),
+        },
+      });
+      res.send({newUser:newUser, token: getToken(newUser.id)});
     }
-    
+  } catch (error) {
+    // @ts-ignore
+    res.status(400).send({ error: error.message });
+  }
+});
+app.post("/sign-in", async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { email: req.body.email },
+  });
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.send({user:user , token:getToken(user.id)});
+  } else {
+    res.status(400).send({ error: "Invalid email/password" });
+  }
+});
+app.get('/validate', async (req,res)=>{
+    try{
+        if(req.headers.authorization){
+            const user = await getCurrentUser(req.headers.authorization);
+            // @ts-ignore
+            res.send({user, token : getToken(user.id)})
 
-})
-
-
-
-
-
-
-app.listen(port, () =>{
-    console.log(`yay: http://localhost:${port}`)
-})
+        } 
+    }catch (error){
+            // @ts-ignore
+           res.status(400).send({error:error.message})
+        }
+    }
+);
+app.listen(port, () => {
+  console.log(`yay: http://localhost:${port}`);
+});

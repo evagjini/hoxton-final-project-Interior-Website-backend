b070ab37173777fc39a5ee8cf39bd1b090805675
Fevronia Gjini <fevronigjini1@gmail.com>
2022-10-13 23:13:30 +0200

    worked on some endpoints

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,66 @@ async function getCurrentUser (token :string){
 
 const port = 5637;
 
-app.get("/users", async (req, res) => {});
+app.get("/blogs", async (req, res) => {
+    try{
+        const blogs = await prisma.blog.findMany({ include:{
+            // @ts-ignore
+            category:true}
+        });
+        res.send(blogs)
+    } catch (error){
+        // @ts-ignore
+        res.status(400).send({errors:error.message})
+    }
+});
+app.get('/blog/:id', async (req,res)=>{
+  try{
+    const blogId = Number(req.params.id)
+    const blog = await prisma.blog.findUnique({
+      where:{id: blogId},
+      include:{designer :true, likes:true, comments:{include:{user:true}}}
+    });
+    if (blog){
+      res.send(blog);
+    } else{
+      res.send(404).send({error : 'Blog not Found!'});
+      }
+    } catch(error){
+      // @ts-ignore 
+      res.status(400).send({error:error.message})
+    }
+  }
+)
+app.get('/categories',async (req,res)=>{
+    try{
+        const catogories = await prisma.category.findMany({ include:
+            {blogs:true}}
+) ;
+res.send(catogories)
+        } catch(error){
+            // @ts-ignore
+            res.status(400).send({error:error.message})
+        }})
+
+app.get('/categories/:id', async (req,res) =>{
+  try{
+    const id = Number(req.params.id);
+
+    const category = await prisma.category.findUnique({
+       where:{id:id},
+       include:{blogs:true}
+    });
+    if(category){
+      res.send(category)
+    } else {
+      res.status(400).send({error:'Category not Found!'})
+    }
+  } catch (error){
+    // @ts-ignore
+    res.status(400).send({error:error.message})
+  }
+} )
+
 
 app.post("/sign-up", async (req, res) => {
   try {
@@ -82,5 +141,5 @@ app.get('/validate', async (req,res)=>{
     }
 );
 app.listen(port, () => {
-  console.log(`yay: http://localhost:${port}`);
-});
+  console.log(`yay: http://localhost:${port}`)
+})
